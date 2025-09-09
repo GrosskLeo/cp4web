da841f23c7a6fbf32129d9193931a78aeed1fe01
GrosskLeo <grosskleo@gmail.com>
2025-09-09 09:13:10 -0300

    TodoForm]

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Date.now(), text }]);
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    
-  )
+    <div>
+      <h1>To-do List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} removeTodo={removeTodo} />
+    </div>
+  );
 }
 
-export default App
+export default App;
