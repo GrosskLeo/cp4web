bb9011b316045ca9d3dc032f1477311cce981d75
GrosskLeo <grosskleo@gmail.com>
2025-09-09 09:17:04 -0300

    TodoItem

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,10 @@
+function TodoItem({ todo, removeTodo }) {
+  return (
+    <li>
+      {todo.text}
+      <button onClick={() => removeTodo(todo.id)}>X</button>
+    </li>
+  );
+}
+
+export default TodoItem;
