a7d8faa42afcc9c52bdebff09de0ca75ddde0fa0
GrosskLeo <grosskleo@gmail.com>
2025-09-09 09:55:26 -0300

    Finalizando

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.Stri 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+ 
